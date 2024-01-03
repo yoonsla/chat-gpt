60edf3779ab967436ec69b3443c85a1fa462c0e4
yoonsla <sy.kim@twolinecode.com>
2024-01-03 15:33:57 +0900

    response 추가

--- src/main/java/com/example/chatgpt/controller/ChatController.java
+++ src/main/java/com/example/chatgpt/controller/ChatController.java
@@ -1,8 +1,8 @@
 package com.example.chatgpt.controller;
 
-import com.example.chatgpt.dto.ChatGptRequestDto;
 import com.example.chatgpt.dto.ChatGptResponseDto;
 import com.example.chatgpt.dto.QuestionRequestDto;
+import com.example.chatgpt.response.Response;
 import com.example.chatgpt.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,13 +19,13 @@ public class ChatController {
 
     // sdk
     @PostMapping("")
-    public String getChat(@RequestBody QuestionRequestDto requestDto) {
-        return chatService.getChat(requestDto);
+    public Response<String> getChat(@RequestBody QuestionRequestDto requestDto) {
+        return Response.from(chatService.getChat(requestDto));
     }
 
     // rest api
     @PostMapping("/question")
-    public ChatGptResponseDto sendQuestion(@RequestBody QuestionRequestDto requestDto) {
-        return chatService.askQuestion(requestDto);
+    public Response<ChatGptResponseDto> sendQuestion(@RequestBody QuestionRequestDto requestDto) {
+        return Response.from(chatService.askQuestion(requestDto));
     }
 }
--- src/main/java/com/example/chatgpt/dto/ChatGptResponseDto.java
+++ src/main/java/com/example/chatgpt/dto/ChatGptResponseDto.java
@@ -1,6 +1,6 @@
 package com.example.chatgpt.dto;
 
-import io.github.flashvayne.chatgpt.dto.Choice;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -15,4 +15,16 @@ public class ChatGptResponseDto {
     private LocalDate created;
     private String model;
     private List<Choice> choices;
+
+    @AllArgsConstructor
+    @Getter
+    public static class Choice {
+
+        private String text;
+        private Integer index;
+        private String logprobs;
+
+        @JsonProperty("finish_reason")
+        private String finishReason;
+    }
 }
